60f42da86aaa2e4aa7b5afe1ee015ecc985f95be
HwH0211 <1605779817@qq.com>
2023-11-11 12:07:39 +0800

    OJ判题系统前端第一次提交

---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="userLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <a-space>
+          <img src="../assets/oj-logo.svg" class="logo" />
+          <div>蜜瓜 OJ</div>
+        </a-space>
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer"> 代码练习平台</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+<style scoped>
+#userLayout {
+  text-align: center;
+}
+
+#userLayout .header {
+  margin-top: 16px;
+  margin-bottom: 16px;
+}
+
+#userLayout .content {
+  margin-bottom: 16px;
+  padding: 20px;
+}
+
+#userLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: sticky;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+.logo {
+  height: 55px;
+}
+</style>
+<script setup lang="ts"></script>
